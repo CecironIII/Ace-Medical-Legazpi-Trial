87ac8f5d3e52db6a5b84e2e6a22eb121151fb3c3
ceci <42309272+CecironIII@users.noreply.github.com>
2019-03-31 21:07:01 +0800

    initial commit

---
+++ script/main.js
@@ -0,0 +1,54 @@
+$(window).scroll(function() {
+    $('#navbar').toggleClass('navbar-scrolled', $(this).scrollTop() > 170);
+});
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 50 ) {
+      $('.scrolltop:hidden').stop(true, true).fadeIn();
+  } else {
+      $('.scrolltop').stop(true, true).fadeOut();
+  }
+  });
+
+$(function(){
+  $(".scroll").click(function(){
+    $("html,body").animate({
+      scrollTop:$('.container-fluid').offset().top});
+      return false})});
+
+      // Select all links with hashes
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) { // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+        };
+      });
+    }
+  }
+});
